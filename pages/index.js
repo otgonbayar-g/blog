df50449e72a6e7197392c5737eb0569d340ae9f2
Otgonbayar <otgonbayar.g@gmail.com>
2023-03-08 15:42:44 +0800

    Add react-bootstrap

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,29 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Alert from 'react-bootstrap/Alert';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <div className={styles.container}>My blog</div>
+  return (
+    <div className={styles.main}>
+      <>
+        {[
+          'primary',
+          'secondary',
+          'success',
+          'danger',
+          'warning',
+          'info',
+          'light',
+          'dark',
+        ].map((variant) => (
+          <Alert key={variant} variant={variant}>
+            This is a {variant} alert—check it out!
+          </Alert>
+        ))}
+      </>
+    </div>
+  );
 }
